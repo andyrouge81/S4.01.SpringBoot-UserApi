df641b08ec7710fc37a4ab52378d0195cc9ef56b
andyrouge81 <andyrouge81@gmail.com>
2025-12-09 19:38:40 +0100

    feat(user): in memory user list

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,20 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private static List<String> users = new ArrayList<>();
+
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return new ArrayList<>();
+    }
+}
--- src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
@@ -1,11 +1,11 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.level1.controllers.HealthController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
