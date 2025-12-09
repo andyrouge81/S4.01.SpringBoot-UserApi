ef925922df650ff548a68d9e4b5faaca8b75c6db
andyrouge81 <andyrouge81@gmail.com>
2025-12-09 20:18:32 +0100

    feat(user): add POST /user endpoint  to create users

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -2,19 +2,29 @@ package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.entities.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
 
-    private static List<String> users = new ArrayList<>();
+    private static List<User> users = new ArrayList<>();
 
 
     @GetMapping("/users")
     public List<User> getUsers(){
-        return new ArrayList<>();
+        return users;
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user){
+        user.setId(UUID.randomUUID());
+        users.add(user);
+        return user;
     }
 }
--- src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/HealthControllerTest.java
@@ -1,6 +1,6 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
-import cat.itacademy.s04.t01.userapi.level1.controllers.HealthController;
+import cat.itacademy.s04.t01.userapi.controllers.HealthController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
