efa878c698c507b4294dabc095e69d0c56be84be
andyrouge81 <andyrouge81@gmail.com>
2025-12-09 22:28:10 +0100

    feat(user):add GET /user/{id} endpoint to find user

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,14 +1,15 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.entities.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
 
 @RestController
 public class UserController {
@@ -26,5 +27,13 @@ public class UserController {
         user.setId(UUID.randomUUID());
         users.add(user);
         return user;
+    }
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable UUID id){
+        return users.stream()
+                .filter(u->u.getId().equals(id))
+                .findFirst()
+                .orElseThrow(()-> new UserNotFoundException("NotFound(404)"));
+
     }
 }
