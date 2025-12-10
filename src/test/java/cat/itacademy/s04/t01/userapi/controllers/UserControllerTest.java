3b4d05fc9f225cc53bcffb3630bb6083799d9a9e
andyrouge81 <andyrouge81@gmail.com>
2025-12-10 16:38:56 +0100

    test(user): adding two users contains 'jo' inside name value

--- src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import static org.hamcrest.core.StringContains.containsStringIgnoringCase;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -98,6 +99,45 @@ class UserControllerTest {
 
         mockMvc.perform(get("/users/{id}", id))
                 .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    void getUsers_withNameParam_returnsFilteredUsers() throws Exception {
+
+        Map<String, String> body = new HashMap<>();
+        body.put("name", "Charjo");
+        body.put("email", "coxis@example.com");
+
+
+
+        String jsonBody = mapper.writeValueAsString(body);
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody))
+                        .andExpect(status().isOk());
+
+        Map<String, String> body2 = new HashMap<>();
+        body2.put("name", "jolian");
+        body2.put("email", "folixa@example.com");
+
+        String jsonBody2 = mapper.writeValueAsString(body2);
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody2))
+                        .andExpect(status().isOk());
+
+
+
+        mockMvc.perform(get("/users").param("name","jo"))
+
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].name", containsStringIgnoringCase("jo")))
+                .andExpect(jsonPath("$[1].name", containsStringIgnoringCase("jo")))
+                .andExpect(jsonPath("$.length()").value(2));
+
 
     }
 
