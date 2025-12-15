8d21739a28ae779ebac1b044b2ff4104f0e97fa2
andyrouge81 <andyrouge81@gmail.com>
2025-12-15 17:54:16 +0100

    test(user): refactor UserControllerTest with MockBean userService

--- src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
@@ -1,26 +1,31 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.entities.User;
-import org.junit.jupiter.api.BeforeEach;
+import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
+import cat.itacademy.s04.t01.userapi.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import tools.jackson.databind.ObjectMapper;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.core.StringContains.containsStringIgnoringCase;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
+    @MockitoBean
+    private UserService userService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -28,10 +33,7 @@ class UserControllerTest {
     @Autowired
     private ObjectMapper mapper;
 
-    @BeforeEach
-    void setup(){
-        UserController.clearUsers();
-    }
+
 
     @Test
     void getUsers_returnEmptyListInitially() throws Exception{
@@ -43,13 +45,17 @@ class UserControllerTest {
     }
 
     @Test
-    void createUser_returnsUserWithId() throws Exception {
+    void createUser_returnsUserFromService() throws Exception {
 
-        Map<String, String> body = new HashMap<>();
-        body.put("name", "Pepito");
-        body.put("email", "pepito@example.com");
-        String jsonBody = mapper.writeValueAsString(body);
+        //given
+        User user = new User(UUID.randomUUID(), "Pepito","pepito@example.com");
+        String jsonBody = mapper.writeValueAsString(user);
 
+        //when
+        when(userService.createUser(any(User.class)))
+                .thenReturn(user);
+
+        //then
         mockMvc.perform(post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonBody))
@@ -60,28 +66,20 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.name").value("Pepito"))
                 .andExpect(jsonPath("$.email").value("pepito@example.com"));
 
-
     }
 
 
     @Test
-    void getUserById_returnsCorrectUser() throws Exception {
-        Map<String, String> body = new HashMap<>();
-        body.put("name", "choripan");
-        body.put("email", "choripa@example.com");
-        String jsonBody = mapper.writeValueAsString(body);
+    void getUserById_returnsUserFromService() throws Exception {
 
-        MvcResult result = mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonBody))
+        UUID id = UUID.randomUUID();
+        User user1 = new User(id
+                ,"choripan", "choripa@example.com" );
 
-                .andExpect(status().isOk())
-                .andReturn();
 
-        String responseJson = result.getResponse().getContentAsString();
-        User createUser = mapper.readValue(responseJson, User.class);
+        when(userService.searchUserById(id))
+                .thenReturn(user1);
 
-        UUID id = createUser.getId();
 
         mockMvc.perform(get("/users/{id}", id))
                 .andExpect(status().isOk())
@@ -91,19 +89,12 @@ class UserControllerTest {
 
     @Test
     void getUserById_returnsNotFoundIfMissing() throws Exception{
-        Map<String, String> body = new HashMap<>();
-        body.put("name", "ramon");
-        body.put("email", "ramoncin@example.com");
 
-        String jsonBody = mapper.writeValueAsString(body);
-
-        mockMvc.perform(post("/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                 .content(jsonBody))
-                .andExpect(status().isOk());
 
         UUID id = UUID.randomUUID();
 
+        when(userService.searchUserById(id))
+                .thenThrow(new UserNotFoundException(" User Not Found"));
 
         mockMvc.perform(get("/users/{id}", id))
                 .andExpect(status().isNotFound());
@@ -113,40 +104,33 @@ class UserControllerTest {
     @Test
     void getUsers_withNameParam_returnsFilteredUsers() throws Exception {
 
-        Map<String, String> body = new HashMap<>();
-        body.put("name", "Charjo");
-        body.put("email", "coxis@example.com");
+        List<User> usersList = List.of(new User(UUID.randomUUID(), "Charjo", "charjo@example.com"),
+                new User(UUID.randomUUID(), "Jolian", "jolijoli@example.com"));
 
-
-
-        String jsonBody = mapper.writeValueAsString(body);
-
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonBody))
-                        .andExpect(status().isOk());
-
-        Map<String, String> body2 = new HashMap<>();
-        body2.put("name", "jolian");
-        body2.put("email", "folixa@example.com");
-
-        String jsonBody2 = mapper.writeValueAsString(body2);
-
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonBody2))
-                        .andExpect(status().isOk());
-
+        when(userService.searchUserByName("jo"))
+                .thenReturn(usersList);
 
 
         mockMvc.perform(get("/users").param("name","jo"))
-
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$[0].name", containsStringIgnoringCase("jo")))
                 .andExpect(jsonPath("$[1].name", containsStringIgnoringCase("jo")))
                 .andExpect(jsonPath("$.length()").value(2));
 
 
+    }
+
+    @Test
+    void getAllUsers_withoutNameParam_getAllUsers() throws Exception{
+
+        when(userService.getAllUsers())
+                .thenReturn(List.of());
+
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk());
+
+        Mockito.verify(userService).getAllUsers();
     }
 
 }
