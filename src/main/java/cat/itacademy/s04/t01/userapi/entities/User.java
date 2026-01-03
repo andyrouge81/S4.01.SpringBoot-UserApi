a4a5325ab155c928cee222bd883ccaf627117519
andyrouge81 <andyrouge81@gmail.com>
2026-01-03 11:03:42 +0100

    feat(user):adding default constructor for Jackson serialize

--- src/main/java/cat/itacademy/s04/t01/userapi/entities/User.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/entities/User.java
@@ -7,6 +7,9 @@ public class User {
     private String name;
     private String email;
 
+    public User(){
+
+    }
     public User(UUID id, String name, String email) {
         this.id = id;
         this.name = name;
